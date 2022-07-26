724a2bd258f83813e3b2b97f462dcb6dc14a907e
Izza wildan ridhoni <izzawildan88@gmail.com>
2022-07-26 11:17:19 +0700

    update darkmode + perbaikan fitur

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,10 +2,15 @@
 window.onscroll = function () {
   const header = document.querySelector('header');
   const fixedNav = header.offsetTop;
+  const toTop = document.querySelector('#to-top');
 
   if (window.pageYOffset > fixedNav) {
     header.classList.add('navbar-fixed');
+    toTop.classList.remove('hidden');
+    toTop.classList.add('fixed');
   } else {
+    toTop.classList.remove('fixed');
+    toTop.classList.add('hidden');
     header.classList.remove('navbar-fixed');
   }
 };
@@ -18,3 +23,32 @@ hamburger.addEventListener('click', function () {
   hamburger.classList.toggle('hamburger-active');
   navMenu.classList.toggle('hidden');
 });
+
+// klik diluar humberger maka akan ke close
+window.addEventListener('click', function (e) {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove('hamburger-active');
+    navMenu.classList.add('hidden');
+  }
+});
+
+// Darkmode Toggle
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', function () {
+  if (darkToggle.checked) {
+    html.classList.add('dark');
+    localStorage.theme = 'dark';
+  } else {
+    html.classList.remove('dark');
+    localStorage.theme = 'light';
+  }
+});
+
+// pindahkan posisi toggle sesuia mode
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
+}
